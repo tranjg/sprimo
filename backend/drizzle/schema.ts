86449050912f188907ca49b93717cdde20b42302
tranjg <justintranthedeveloper@gmail.com>
2025-06-08 12:21:12 -0400

    added teams to schema

--- backend/drizzle/schema.ts
+++ backend/drizzle/schema.ts
@@ -1,8 +1,8 @@
-import { date, pgTable, varchar } from "drizzle-orm/pg-core";
+import { date, pgTable, uuid, varchar } from "drizzle-orm/pg-core";
 
-export const users = pgTable('users', {
-    id: varchar('id',{length:20}).notNull().primaryKey(),
-    username:varchar('username',{length:50}).notNull(),
-    password:varchar('password',{length:256}).notNull(),
-    createdAt:date('created_at').defaultNow()
-});
+export const teams = pgTable('teams', {
+    id: uuid('id').defaultRandom().primaryKey(),
+    name: varchar('name', {length: 50}).notNull(),
+    jira_board_id: varchar('jira_board_id'),
+    github_repo_url: varchar('github_repo_url'),
+})
