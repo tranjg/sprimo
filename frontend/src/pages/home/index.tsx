6b1152dee6ff46ff563fe6a5d0710a177fe95dad
justin <justintranthedeveloper@gmail.com>
2025-06-10 02:58:52 +0000

    changed app theme

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <Layout>
       <div className="flex h-full w-full justify-center items-center">
-        <h1 className="font-semibold">Welcome to Sprimo</h1>
+        <h1 className="font-semibold text-primary">Welcome to Sprimo</h1>
       </div>
     </Layout>
   );
