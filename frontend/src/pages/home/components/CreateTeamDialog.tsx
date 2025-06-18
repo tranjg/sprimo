f63a6626883dffc9016ede1338efa543221d46de
tranjg <justintranthedeveloper@gmail.com>
2025-06-18 01:24:43 -0400

    added input tags for add team; put add team dialog into component;

---
+++ frontend/src/pages/home/components/CreateTeamDialog.tsx
@@ -0,0 +1,50 @@
+import { InputTags } from "@/components/InputTags"
+import { Button } from "@/components/ui/button"
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Textarea } from "@/components/ui/textarea"
+import { PlusIcon } from "lucide-react"
+import { useState } from "react"
+
+const CreateTeamDialog = () => {
+    const [members, setMembers] = useState<string[]>([]);
+    return(
+        <Dialog>
+          <DialogTrigger>
+            <Button>
+              Create Team
+              <PlusIcon />
+            </Button>
+            </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Create Team</DialogTitle>
+              <DialogDescription>
+              Create a team to track sprint progress and health.
+              Add members, assign roles, and set a team lead to start monitoring key sprint metrics and improving performance.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="flex flex-col items-start gap-2">
+              <div className="grid flex-1 gap-2 w-full">
+                <Label>Team Name:</Label>
+                <Input />
+              </div>
+              <div className="grid flex-1 gap-2 w-full">
+                <Label>Team Description:</Label>
+                <Textarea />
+              </div>
+              <div className="grid flex-1 gap-2 w-full">
+                <Label>Add Members:</Label>
+                <InputTags value={members} onChange={setMembers}/>
+              </div>
+            </div>
+          <DialogFooter>
+            <Button type="submit">Save</Button>
+          </DialogFooter>
+          </DialogContent>
+        </Dialog>
+    )
+}
+
+export default CreateTeamDialog
--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -8,42 +8,19 @@ import { VelocityChart } from "../../components/VelocityChart.tsx";
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog.tsx";
 import { Input } from "@/components/ui/input.tsx";
 import { Label } from "@/components/ui/label.tsx";
+import { Textarea } from "@/components/ui/textarea.tsx";
+import { InputTags } from "@/components/InputTags.tsx";
+import { useState } from "react";
+import CreateTeamDialog from "./components/CreateTeamDialog.tsx";
 
 function Home() {
+  const [members, setMembers] = useState<string[]>([])
+
   return (
     <Layout>
       <div className="flex-1 p-2">
         <div className="flex p-4 border-1 rounded-md justify-end bg-sidebar">
-        <Dialog>
-          <DialogTrigger>
-            <Button>
-              Create Team
-              <PlusIcon />
-            </Button>
-            </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>Create Team</DialogTitle>
-              <DialogDescription>
-              Create a team to track sprint progress and health.
-              Add members, assign roles, and set a team lead to start monitoring key sprint metrics and improving performance.
-              </DialogDescription>
-            </DialogHeader>
-            <div className="flex flex-col items-start gap-2">
-              <div className="grid flex-1 gap-2">
-                <Label>Team Name:</Label>
-                <Input />
-              </div>
-              <div className="grid flex-1 gap-2">
-                <Label>Team Lead:</Label>
-                <Input />
-              </div>
-            </div>
-          <DialogFooter>
-            <Button type="submit">Save</Button>
-          </DialogFooter>
-          </DialogContent>
-        </Dialog>
+        <CreateTeamDialog />
         </div>
         <div className="flex lg:flex-row flex-col gap-4 mt-4 p-4 border-1 rounded-md h-full">
           <div className="flex flex-col basis-1/2 p-4 gap-4 h-full">
