19dfcd9a769d0d12059e8c05b65489264ef83a67
tranjg <justintranthedeveloper@gmail.com>
2025-06-17 20:15:44 -0400

    added create team dialog

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -5,18 +5,44 @@ import { SprintBurndownChart } from "../../components/SprintBurndownChart.tsx";
 import { WorkItemFlowChart } from "../../components/WorkItemFlowChart.tsx";
 import { SprintGoalCompletionChart } from "../../components/SprintGoalCompletionChart.tsx";
 import { VelocityChart } from "../../components/VelocityChart.tsx";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog.tsx";
+import { Input } from "@/components/ui/input.tsx";
+import { Label } from "@/components/ui/label.tsx";
 
 function Home() {
   return (
     <Layout>
       <div className="flex-1 p-2">
         <div className="flex p-4 border-1 rounded-md justify-end bg-sidebar">
-          <Button>
-            Create Team
-            <PlusIcon />
-          </Button>
+        <Dialog>
+          <DialogTrigger>
+            <Button>
+              Create Team
+              <PlusIcon />
+            </Button>
+            </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Create Team</DialogTitle>
+              <DialogDescription>
+              Create a team to track sprint progress and health.
+              Add members, assign roles, and set a team lead to start monitoring key sprint metrics and improving performance.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="flex flex-col items-start gap-2">
+              <div className="grid flex-1 gap-2">
+                <Label>Team Name:</Label>
+                <Input />
+              </div>
+              <div className="grid flex-1 gap-2">
+                <Label>Team Lead:</Label>
+                <Input />
+              </div>
+            </div>
+          </DialogContent>
+        </Dialog>
         </div>
-        <div className="flex flex-row gap-4 mt-4 p-4 border-1 rounded-md h-full">
+        <div className="flex lg:flex-row flex-col gap-4 mt-4 p-4 border-1 rounded-md h-full">
           <div className="flex flex-col basis-1/2 p-4 gap-4 h-full">
             <VelocityChart />
             <SprintGoalCompletionChart />
