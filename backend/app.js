0cb39537406b915efb13bbf762721c886d803314
tranjg <justintranthedeveloper@gmail.com>
2025-06-20 02:16:10 -0400

    changed app.js (express)

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,9 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/api', (req, res) => {
+  res.send('Sprimo Backend')
 })
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Sprimo Backend started on port ${port}`)
 })
