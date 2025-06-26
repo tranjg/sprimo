40d8bfcf93025ce45db699419d15842bc03258eb
tranjg <justintranthedeveloper@gmail.com>
2025-06-26 17:53:39 -0400

    imported correct provider for redux

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { router } from "./routes/routes.tsx";
 import { Toaster } from "./components/ui/sonner.tsx";
 import AuthProvider from "./providers/authProvider.tsx";
-import { Provider } from "@radix-ui/react-tooltip";
+import { Provider } from "react-redux";
 import { store } from "./reducers/store.ts";
 
 createRoot(document.getElementById("root")!).render(
