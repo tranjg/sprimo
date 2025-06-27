5771b3245cd5a1e708b475512a17e2e5f9745e07
tranjg <justintranthedeveloper@gmail.com>
2025-06-26 21:37:38 -0400

    redirect login to dashboard

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,6 +1,26 @@
+import { useNavigate } from "react-router-dom"
 import LoginForm from "./components/LoginForm"
+import { useEffect, useState } from "react"
+
 
 const Login = () => {
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const token = localStorage.getItem("token")
+        if (token) {
+            navigate("/dashboard")
+        } else {
+            setLoading(false)
+        }
+    }, [navigate])
+
+    if (loading) {
+        return(
+            <h1>Loading...</h1>
+        )
+    }
     return(
         <div className="flex justify-center items-center w-screen h-screen border-1">
             <div className="flex justify-center items-center w-full h-full">
@@ -9,5 +29,6 @@ const Login = () => {
         </div>
     )
 }
+
 
 export default Login
