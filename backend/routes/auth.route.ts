798d96405577896cfc8f640d51ff9cfd37cc4785
tranjg <justintranthedeveloper@gmail.com>
2025-06-29 00:26:21 -0400

    updated import name

--- backend/routes/auth.route.ts
+++ backend/routes/auth.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { register, login } from "../controllers/auth.controller.js";
+import { register, login } from "../controllers/auth.controller.ts";
 
 const router = express.Router();
 
--- backend/routes/team.route.ts
+++ backend/routes/team.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { addTeam } from "../controllers/team.controller";
+import { addTeam } from "../controllers/team.controller.ts";
 
 const router = express.Router();
 
