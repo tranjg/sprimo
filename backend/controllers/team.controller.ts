e34754ffb15d1dd1582ab11c1f87b087192e5240
tranjg <justintranthedeveloper@gmail.com>
2025-07-10 00:08:40 -0400

    added memberCount and projects to getTeams

--- backend/controllers/team.controller.ts
+++ backend/controllers/team.controller.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/node-postgres"
-import { eq } from "drizzle-orm"
-import { team_members, teams } from "../drizzle/schema.ts"
+import { eq, getTableColumns } from "drizzle-orm"
+import { projects, team_members, teams } from "../drizzle/schema.ts"
 
 const db = drizzle(process.env.DATABASE_URL as string)
 
@@ -34,11 +34,16 @@ export const addTeam = async (req, res) => {
 export const getTeams = async (req, res) => {
     try {
         const userId = req.query.userId;
+
+        const memberCount = db.$count(team_members, eq(team_members.team_id, teams.id))
+
         const teamsApartOf = await db
-        .select(teams)
+        .select({...getTableColumns(teams), memberCount, projects: getTableColumns(projects)})
         .from(teams)
         .innerJoin(team_members, eq(team_members.team_id, teams.id))
+        .leftJoin(projects, eq(projects.team_id, teams.id))
         .where(eq(team_members.user_id, userId))
+        .groupBy(teams.id, projects.id)
    
         return res.json(teamsApartOf)
     } catch (error) {
