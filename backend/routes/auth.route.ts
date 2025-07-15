ed4b777188dab712e1c828c333cd24542e3d65e9
tranjg <justintranthedeveloper@gmail.com>
2025-07-15 00:50:12 -0400

    added new auth routes

--- backend/routes/auth.route.ts
+++ backend/routes/auth.route.ts
@@ -1,10 +1,16 @@
 import express from "express";
-import { register, login } from "../controllers/auth.controller.ts";
+import { register, login, authorize, callback, getSessionInfo } from "../controllers/auth.controller.ts";
 
 const router = express.Router();
 
 router.post("/register", register);
 
 router.post("/login", login);
+
+router.get("/authorize", authorize)
+
+router.get("/callback", callback)
+
+router.get("/session", getSessionInfo)
 
 export default router
