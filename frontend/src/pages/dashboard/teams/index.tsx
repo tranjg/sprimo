e94f141bf669eb6738feda06835dcf5d12a758b7
tranjg <justintranthedeveloper@gmail.com>
2025-07-27 13:59:05 -0400

    moved create team dialog to teams page

--- frontend/src/pages/dashboard/teams/index.tsx
+++ frontend/src/pages/dashboard/teams/index.tsx
@@ -1,15 +1,15 @@
-import TeamsList from "./components/TeamsList"
+import CreateTeamDialog from "../components/CreateTeamDialog";
+import TeamsList from "./components/TeamsList";
 
 const Teams = () => {
-    return(
-        <div className="flex-1 p-5">
-            {/* <div className="flex flex-col gap-2 p-4 border-1 rounded-md">
-                <h1 className="font-bold text-xl">Sprimo</h1>
-                <p>This is the description of the team</p>
-            </div> */}
-            <TeamsList />
-        </div>
-    )
-}
+  return (
+    <div className="flex-1 p-5">
+      <div className="flex p-4 mb-3 w-full border-1 rounded-md justify-end bg-sidebar">
+        <CreateTeamDialog />
+      </div>
+      <TeamsList />
+    </div>
+  );
+};
 
-export default Teams
+export default Teams;
